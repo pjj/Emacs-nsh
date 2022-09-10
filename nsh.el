51232237fa6d7700e771b07d0c79d2b47064fc1e
Paul Maragakis <paul.maragakis@deshawresearch.com>
2022-09-10 11:18:32 -0400

    This syncs to my current version, including nsh-in-project.

--- nsh.el
+++ nsh.el
@@ -1,28 +1,26 @@
 ;;; nsh.el --- create a new shell in a named buffer
-;;; Keep separate histories for each shell in ~/Dropbox/nsh_history
+;;; Keep separate histories for each bash shell in you nsh-bash-history-dir
 ;;;
 ;;; Questions: Paul.Maragakis@gmail.com
 ;;;
 ;;; Commentary:
 ;;; This is a tool that I've used for over a decade.
-;;; The only part that is no longer meaningful to me is syncing via Dropbox,
-;;; but the code works as expected with any bash-history-dir.
 ;;;
 
 ;;; Code:
 
 (require 'shell)
 
-
 ;; Customization helps select useful buffers for default split functionality.
 (defgroup nsh-customization
   '(
     (nsh-bash-executable custom-variable) ;; location of bash executable
     (nsh-bash-history-dir custom-variable) ;; directory that contains all past shell histories
+    (nsh-common-env custom-variable)
     )
-  "Customization group for nsh.el."
+  "Customization group for split-frame.el."
   :group 'convenience
-  :prefix "nsh")
+  :prefix "split-frame")
 
 (defcustom nsh-bash-executable
   "/bin/bash"
@@ -37,39 +35,52 @@
 	 (b (file-accessible-directory-p a)))
     (when b a)))
 
-;; We will keep all past histories in directory bash-history-dir
+;; We will keep all past histories in directory nsh-bash-history-dir
 ;; The default location is a personal choice that is customizable
 (defcustom nsh-bash-history-dir
   (or
-   (getenv "NSHELLDIR")
+   (getenv "NSHDIR")
    (expand-file-name-if-dir-accessible "~/Dropbox/nsh_history/")
    (expand-file-name-if-dir-accessible "~/Dropbox/.nsh_history/")
    (expand-file-name-if-dir-accessible "~/.emacs.d/nsh_history/")
    (expand-file-name-if-dir-accessible "~/.emacs.d/.nsh_history/")
    (file-name-as-directory (expand-file-name "~/.nsh_history/")))
-  "The directory that contains all past shell histories."
+  "The directory that contains all past shell histories (name should end in /)."
   :group 'nsh-customization
   :type '(string))
 
-;; ;; In all my non-windows machines, bash is here:
-;; (defconst bash-executable "/bin/bash")
+;; Custom environment association list.
+;; Every new shell sets these common history-related variables
+;; and then also sets a different HISTFILE.
+;; If you need any additional overrides to the default bash,
+;; you can add them to this list.
+(defcustom nsh-bash-common-env
+  '(("HISTSIZE" . "50000")              ; recall up to 50K commands per shell
+    ("HISTFILESIZE" . "1000000")        ; save all history
+    ("HISTCONTROL" . "ignoredups")      ; ignore repeated entries
+    ("HISTIGNORE" . "ls:ls -lrt:bg:fg") ; ignore ls, bg, fg
+    ("HISTTIMEFORMAT" . "%Y-%m-%d %T ") ; show history times
+    ("PROMPT_COMMAND" . "history -a")   ; append history after each command.
+    )
+  "The common environment in the shells."
+  :group 'nsh-customization
+  :type '(alist :key-type (string :tag "Key")
+                :value-type (string :tag "Value")))
 
+;; We use setenv in a 'cl-loop for' to assign all pairs of environment variables.
+(defun nsh-env-setup (nsh-name)
+  "Setup the common environment of nsh NSH-NAME."
+  (require 'cl-lib)
+  (let ()
+    (cl-loop for (key . value) in nsh-bash-common-env do
+	   (setenv key value))
+    (setenv "HISTFILE"
+            (concat nsh-bash-history-dir nsh-name))))
 
-;; (defconst bash-history-dir
-;;   (file-name-as-directory (expand-file-name "~/Dropbox/nsh_history/")))
-
-;; create history directory, if it doesn't already exist.
-(condition-case err
-    ;; first test if directory exists and is readable
-    (or (file-accessible-directory-p nsh-bash-history-dir)
-	(and (file-exists-p nsh-bash-history-dir)         ; exists, but not readable
-	     (set-file-modes nsh-bash-history-dir ?\755)) ;   fix permissions
-	(make-directory nsh-bash-history-dir))            ; it wasn't there, make it
-  ('error (message "From: %s --> %s"                  ; for the unknown error...
-		   load-file-name (error-message-string err))))
-
-;; Implementation of nsh, assuming bash-history-dir exists
-;; In addition to creating a shell, nsh sets a bunch of bash-history variables
+;; Implementation of nsh, assuming nsh-bash-history-dir exists
+;; First select or find a shell name, posssibly reusing an old one,
+;; then set a bunch of bash-history variables and create the shell.
+;; If an nsh with this name already exists, simply select that buffer.
 (defun nsh (nsh-name)
   "Create a new shell in a named buffer NSH-NAME with separate history."
   ;; auto fill the interactive prompt with the names of past shells, but
@@ -79,22 +90,38 @@
                                  (directory-files nsh-bash-history-dir
                                                   nil "^[^\.]"))))
   ;; Set these environment variables for the bash shell that follows
-  ;; If all else fails, please set these variables within your shell.
-  (setenv "HISTFILE"
-          (concat nsh-bash-history-dir nsh-name))
-  (setenv "HISTSIZE" "50000")              ; recall up to 50K commands per shell
-  ;; Note that setting HISTSIZE above also sets comint-input-ring-size to 50K.
-  (setenv "HISTFILESIZE" "1000000")        ; save all history
-  (setenv "HISTCONTROL" "ignoredups")      ; ignore repeated entries
-  (setenv "HISTIGNORE" "ls:ls -lrt:bg:fg") ; ignore ls, bg, fg
-  (setenv "HISTTIMEFORMAT" "%Y-%m-%d %T ") ; show history times
-  ;;(setenv "HISTTIMEFORMAT" "%F %T ")       ; show history times
-  (setenv "PROMPT_COMMAND" "history -a")   ; append history after each command.
   ;; Create the bash shell in the named buffer
   (let ((nsh-buffer (get-buffer-create (format "nsh-%s" nsh-name)))
         (explicit-shell-file-name nsh-bash-executable))
-    (shell nsh-buffer))
-)
+    (nsh-env-setup nsh-name)
+    (shell nsh-buffer)))
+
+;; Because project.el has become rather useful over the last decade,
+;; I provide here an implementation of nsh-in-project, which starts
+;; an automatically named shell in the root directory of the project.
+;; I typically customize project-switch-commands and add nsh-in-project.
+(defun nsh-in-project ()
+  "Create a new shell in project with default name and separate history."
+  (interactive)
+  (require 'project)
+  (let* ((default-directory (project-root (project-current t)))
+	 (proj-name (file-name-nondirectory
+		     (directory-file-name default-directory)))
+	 (nsh-name (format "proj-%s" proj-name))
+	 (nsh-buffer (get-buffer-create (format "nsh-%s" nsh-name)))
+	 (explicit-shell-file-name nsh-bash-executable))
+    (nsh-env-setup nsh-name)
+    (shell nsh-buffer)))
+
+;; create history directory, if it doesn't already exist.
+(condition-case err
+    ;; first test if directory exists and is readable
+    (or (file-accessible-directory-p nsh-bash-history-dir)
+	(and (file-exists-p nsh-bash-history-dir)         ; exists, but not readable
+	     (set-file-modes nsh-bash-history-dir ?\700)) ; fix permissions (shells are personal)
+	(make-directory nsh-bash-history-dir))            ; it wasn't there, make it
+  ('error (message "From: %s --> %s"                  ; for the unknown error...
+		   load-file-name (error-message-string err))))
 
 (provide 'nsh)
 ;;; nsh.el ends here
